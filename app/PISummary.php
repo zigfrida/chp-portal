3ee6065d65c2f61fdc78c73b742725743a546af9
Amanda Golubics <amanda.golubics@gmail.com>
2019-04-03 11:17:47 -0700

    Fix new user added to pisummaries table

--- app/PISummary.php
+++ app/PISummary.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class PISummary extends Model
 {
     protected $fillable = [
-        'comment',
+        'comment','user_id',
     ];
 
     protected $hidden = [
-        'user_id', 'units', 'NAVPerUnit', 'NAV', 'cost',
+        'units', 'NAVPerUnit', 'NAV', 'cost',
         'cumulative_pref_distribution', 'month_distribution', 'amount_distribution',
         'year_profit_share',
     ];
