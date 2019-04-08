4889ee35af19f286b7252a415deeec8470835900
Amanda Golubics <amanda.golubics@gmail.com>
2019-04-07 17:23:54 -0700

    Adding favicon to portfolio page.

--- resources/views/layouts/portfolio-master.blade.php
+++ resources/views/layouts/portfolio-master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <link rel="shortcut icon" href="/images/favicon.ico">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>Portfolio</title>
