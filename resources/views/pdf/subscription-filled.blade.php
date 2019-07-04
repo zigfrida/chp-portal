5d1a87f788ab39dc3300c7932ffca1246e4c45dc
momo <normalguyinvancouver@gmail.com>
2019-07-04 16:55:26 -0700

    test if initials img works

--- resources/views/pdf/subscription-filled.blade.php
+++ resources/views/pdf/subscription-filled.blade.php
@@ -1163,7 +1163,7 @@ The Subscriber acknowledges that the Issuer is relying upon the Subscriber's dis
         <h5 class="subtitle is-5 has-text-weight-bold">WARNING!</h5>
         <h6 class="subtitle is-6 has-text-weight-bold">This investment is risky. Don't invest unless you can afford to lose all the money you pay for this investment</h6>            
 </div>
-<br><br><br><br><br><br><br><br><br>
+<br><br>
 <div class="content ">
     <div>
         <br>
