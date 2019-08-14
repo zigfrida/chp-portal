0e63b9d1968acdecd7a601dedf2ce0230a7ab19e
momo <normalguyinvancouver@gmail.com>
2019-08-13 19:31:25 -0700

    logo added on login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,10 +25,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{-- {{ config('app.name', 'Laravel') }} --}}
-                    Cypress Hills Partners
-                </a>
+
+                <a id="nav-logo" class="navbar-brand" href="/"><img alt="chp-logo" height="200" width="200" src="/images/cypresshills-black-yellow.png"></a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
