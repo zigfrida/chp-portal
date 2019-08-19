c70b4f0cd6a65c59a1975c8f06b881311f09fdc3
momo <normalguyinvancouver@gmail.com>
2019-08-19 14:14:45 -0700

    testing if clientType works

--- app/Http/Controllers/FormUserController.php
+++ app/Http/Controllers/FormUserController.php
@@ -570,7 +570,58 @@ class FormUserController extends Controller
 
     public function updateProfileCheckboxes(Request $request, $id)
     {
-        dd("hi");
+        dd($request->clientType);
+        if ($request->clientType == 'individual') {
+            \DB::table('form_users')
+                ->where('user_id', $id)
+                ->update(['subscriber_name' => $request->subscriber_name,
+                        'clientType' => $request->clientType,
+                        'city' => $request->city,
+                        'province' => $request->province,
+                        'street' => $request->street,
+                        'postal_code' => $request->postal_code,
+                        'country' => $request->country,
+                        'sin' => $request->sin,
+                        'phone' => $request->phone,
+                        'total_investment' => number_format($request->total_investment, 2, ".", ","),
+                        'ind_ck1' => $request->input('ind_ck1') !== null,
+                        'ind_ck2' => $request->input('ind_ck2') !== null,
+                        'ind_ck3' => $request->input('ind_ck3') !== null,
+                        'ind_ck4' => $request->input('ind_ck4') !== null,
+                        'ind_ck5' => $request->input('ind_ck5') !== null,
+                        'ind_ck6' => $request->input('ind_ck6') !== null,
+                        'req_brokerage' => $request->input('req_brokerage'),
+                        'yass_ck' => $request->input('yass_ck') !== null,
+                    ]);
+        } elseif ($request->clientType == 'business') {
+            \DB::table('form_users')
+                ->where('user_id', $id)
+                ->update(['subscriber_name' => $request->subscriber_name,
+                'clientType' => $request->clientType,
+                'city' => $request->city,
+                'province' => $request->province,
+                'street' => $request->street,
+                'postal_code' => $request->postal_code,
+                'country' => $request->country,
+                'phone' => $request->phone,
+                'business_number' => $request->business_number,
+                'signatory_first_name' => $request->signatory_first_name,
+                'official_capacity_or_title_of_authorized_signatory' => $request->official_capacity_or_title_of_authorized_signatory,
+                'signatory_last_name' => $request->signatory_last_name,
+                'total_investment' => number_format($request->total_investment, 2, ".", ","),
+                'bus_ck1' => $request->input('bus_ck1') !== null,
+                'bus_ck2' => $request->input('bus_ck2') !== null,
+                'bus_ck3' => $request->input('bus_ck3') !== null,
+                'bus_ck4' => $request->input('bus_ck4') !== null,
+                'bus_ck5' => $request->input('bus_ck5') !== null,
+                'bus_ck6' => $request->input('bus_ck6') !== null,
+                'bus_ck7' => $request->input('bus_ck7') !== null,
+                'bus_ck8' => $request->input('bus_ck8') !== null,
+                'bus_ck9' => $request->input('bus_ck9') !== null,
+                'bus_ck10' => $request->input('bus_ck10') !== null,
+                'req_brokerage' => $request->input('req_brokerage'),
+                ]);
+        }
     }
 
     /**
